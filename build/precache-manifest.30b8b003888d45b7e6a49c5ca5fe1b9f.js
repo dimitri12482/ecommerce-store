fcd647ef8c688494a25725537b5a596c618f4a30
Dimitri <65912918+dimitri12482@users.noreply.github.com>
2021-01-17 16:34:26 +0000

    Updated files

---
+++ build/precache-manifest.30b8b003888d45b7e6a49c5ca5fe1b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272b72e425478dd2800827932e0f147c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32325bff06a65b347f84",
+    "url": "/static/js/2.c3f8a7f7.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.c3f8a7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d4ad9b10434f1d7f38",
+    "url": "/static/js/main.7171a91d.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
